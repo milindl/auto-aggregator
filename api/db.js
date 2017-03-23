af8861e30c464c96ea1053aa5ed4fc5ab724c08d
milindl <imilindluthra@gmail.com>
2017-03-23 19:34:35 +0530

    Refactor and delete to accomodate the fact that there is no facebook posting interface

--- api/db.js
+++ api/db.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const access_token = "EAACEdEose0cBABzAsGNcDk1uSYGp5k88OmtwEu9T3zzZBnrqo483uFEpWu1Rb6G66a88myHbRfVVrylr5TBIdjGQp6sf9NJB5o5YSe80VbeDG8wDZBdLyYfFghQKMjZCGhvtfHua1IiEZC31uLTCUYMGzCcqShGwr6WXjjZCrCZAR33YJ3ZBynyYqCkEp9bRrYZD"
 const group_id = "1304156202997238";
 
 const express = require("express");
@@ -10,9 +11,7 @@ const router = express.Router();
 const pool = new pg.Pool();
 graph.setAccessToken(access_token);
 graph.setVersion("2.3");
-
 router.get("/posts", (req, res) => {
-  const today = new Date();
   pool.connect((err, client, done) => {
     if (err) {
       console.log(err);
@@ -23,73 +22,13 @@ router.get("/posts", (req, res) => {
     const query = client.query("SELECT * " +
                                "FROM posts " +
                                "ORDER BY date ASC;");
-    console.log(today);
     query.on("row", row => results.push(row));
     query.on("end", () => {
       done();
+      // console.log(results);
       return res.status(200).json(results);
     });
   });
 });
 
-router.post("/posts", (req, res) => {
-  // TODO: REFACTOR URGENTLY, use PROMISES
-  const message = [ "To: ", req.body.to, "\n",
-                    "From: ", req.body.frm, "\n",
-                    "Date: ", new Date(req.body.date).toUTCString()
-                                                     .split(" ")
-                                                     .slice(0, 4)
-                                                     .join(" "),
-                    "\n",
-                    "Time: ", req.body.time, "\n",
-                    "Additional Info: ", req.body.content, "\n"
-                  ].join("");
-  let postIdPromise = new Promise((resolve, reject) => {
-    graph.post(`${group_id}/feed`, { message: message },
-               (err, result) => {
-                 if(err)
-                   reject(err);
-                 else
-                   resolve(result["id"].split("_")[1]);
-               });
-  });
-  let authorPromise = new Promise((resolve, reject) => {
-    graph.get("/me?fields=name",
-              (err, result) => {
-                if (err)
-                  reject(err);
-                else
-                  resolve(result["name"]);
-              });
-  });
-  Promise.all([postIdPromise, authorPromise])
-    .then((values) => {
-      pool.connect((err, client, done) => {
-        // 'to' is a reserved word and needs to be qouted
-        const query = client.query("INSERT INTO posts(" +
-                                   [
-                                     "author", "content", "date", "frm",
-                                     "\"to\"", "posting_date", "time", "post_id"
-                                   ].join(",") +
-                                   ") values($1, $2, $3, $4, $5, $6, $7, $8)",
-                                   [
-                                     values[1], req.body.content, req.body.date, req.body.frm,
-                                     req.body.to, new Date(), req.body.time, values[0]
-                                   ]);
-        query.on("error", (err) => {
-          done();
-          console.log(err);
-          res.sendStatus(500);
-        });
-        query.on("end", () => {
-          done();
-          res.sendStatus(200);
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    });
-});
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const api = require("./api/db");
 
 const app = express();
-app.use(bodyParser.json());
 app.use("/api", api);
 
 console.log("Starting server...");
